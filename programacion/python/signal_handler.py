a62b2aa780f3e12dd143843b3680679e7c12f38c
Adrian Lopez <adrianlzt@gmail.com>
2017-02-21 18:40:38 +0100

    Auto save
    Former-commit-id: 30fdf58e460e79529b586a90eefc7d9035b90cda

--- programacion/python/signal_handler.py
+++ programacion/python/signal_handler.py
@@ -5,14 +5,14 @@
 import signal
 import sys
 def signal_handler(sgn, frame):
-	print 'You pressed Ctrl+C!'
+	print('You pressed Ctrl+C!')
 	sys.exit(0)
 
 def signal_handler2(sgn, frame):
-        print 'user!'
+        print('user!')
 
 signal.signal(signal.SIGINT, signal_handler) # kill -2 <PID>
 signal.signal(signal.SIGUSR1, signal_handler2) # kill -10 <PID>
 
-print 'Press Ctrl+C'
+print('Press Ctrl+C')
 signal.pause()
