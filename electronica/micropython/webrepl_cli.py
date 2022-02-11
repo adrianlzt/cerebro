826ac0ec222f7e54d33ffa6fc01a9d799fe9500e
Adrian Lopez <adrianlzt@gmail.com>
2022-02-11 20:40:40 +0100

    Auto save

--- electronica/micropython/webrepl_cli.py
+++ electronica/micropython/webrepl_cli.py
@@ -7,18 +7,67 @@
 # Distributed under terms of the MIT license.
 
 """
-Conecta con un webrepl de micropython.
-Envia la contraseña y nos deja listos para enviar o recibir datos.
+Conectar via websockets con el webrepl de micropython.
+El tema de como gestiono el echo no está muy conseguido, así que se ve tanto lo que escribo como
+el echo que me devuelve más la respuesta.
+
+Ejemplo de ejecución:
+    python webrepl_cli.py ws://10.42.0.205:8266 MIPASS
+
+Para cerrar la conex poner "exit"
 """
 
 import websocket
+import sys
+from threading import Thread
 
-def on_message(wsapp, msg):
-    print(msg)
-    if msg == "Password: ":
+# Valores a definir. También podemos pasarlo por la línea de comandos.
+WEBREPL_PASSWORD = "RELLENAR"
+WEBREPL_URL = "ws://IP:PUERTO"
+
+
+def on_message(ws, message):
+    # Print message without newline.
+    print(message, end="")
+    if message == "Password: ":
         print("enviando password")
-        wsapp.send("nomeacuer\r")
+        ws.send(f"{WEBREPL_PASSWORD}\r")
 
-ws = websocket.WebSocketApp("ws://10.42.0.205:8266", on_message=on_message)
-#ws.run_forever()
+def on_error(ws, error):
+    print(error)
 
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+
+def on_open(ws):
+    def run(*args):
+        while True:
+            msg = input()
+            if msg == "exit":
+                break
+            ws.send(msg+'\r')
+
+        ws.close()
+        print("Thread terminating...")
+
+    Thread(target=run).start()
+
+
+if __name__ == "__main__":
+    #websocket.enableTrace(True) # debugging de la conex
+    host = WEBREPL_URL
+
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
+    elif len(sys.argv) == 3:
+        host = sys.argv[1]
+        WEBREPL_PASSWORD = sys.argv[2]
+
+    ws = websocket.WebSocketApp(host,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.on_open = on_open
+    ws.run_forever()
